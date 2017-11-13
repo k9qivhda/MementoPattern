6b483c674655de94cfee7f498dc00f2f7c86e6bd
Victor <kingpin11@infrawareglobal.com>
2017-11-13 11:23:14 +0900

    Create initial project

---
+++ Memento/Memento/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Memento
+//
+//  Created by victor on 2017. 11. 13..
+//  Copyright © 2017년 victor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
